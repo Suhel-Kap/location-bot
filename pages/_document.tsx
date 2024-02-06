7171aadad8f0cced5e58c5c8557fcea75b3a8ef7
Suhel <suhel.kapadia@joinclamp.com>
2024-02-06 21:13:39 +0530

    meta update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
+      <meta
+        name="description"
+        content="This website is for sure gonna make you laugh. It's a collection of jokes and memes. It's a place to have fun and enjoy your time."
+      />
+
       <body>
         <Main />
         <NextScript />
